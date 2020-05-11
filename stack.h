92ba0f5563a02238a4ee7ec12d2674c3a8b54941
Lane W. Surface <surfacelane@gmail.com>
2020-05-11 13:10:36 -0500

    Add fixed stack impl.

--- stack.h
+++ stack.h
@@ -1,25 +1,16 @@
 #pragma once
 
-#ifndef __QUEUE_H_INCLUDED__
-#define __QUEUE_H_INCLUDED__
+#ifndef __STACK_H_INCLUDED__
+#define __STACK_H_INCLUDED__
 
 #include <stdexcept>
 
-/**
- * A first-in last-out container which provides a mechanism for preserving
- * the relative order of elements that are inserted. Adding elements to the
- * queue and then retrieving them at some later point in time ensures that
- * this container provides them in the same order that they were inserted.
- *
- * Use one of the static factory functions, `make_fixed` or `make_resizable`
- * to create an object conforming to this interface.
- */
 template <typename T>
 class stack {
 public:
   virtual ~stack() {}
-  virtual void push(const T elem) = 0;
-  virtual void push(const T elems[], int len) = 0;
+  virtual void push(T elem) = 0;
+  virtual void push(const T elems[], std::size_t len) = 0;
   virtual T pop() = 0;
 
   template <std::size_t SIZE>
@@ -27,83 +18,126 @@ public:
   static stack<T>* make_resizable();
 };
 
+namespace intrnl_ds {
+
 template <typename T, std::size_t SIZE>
 class fixed_stack : public stack<T> {
-  
 public:
-   
+  fixed_stack() : elems(), n(0) {} 
+  virtual ~fixed_stack();
+  virtual void push(T elem) override;
+  virtual void push(const T elems[], std::size_t len) override;
+  virtual T pop() override;
 private:
+  void check_bounds(std::size_t i) const;
   T elems[SIZE]; 
-  std::size_t n{};
+  std::size_t n;
 };
 
 template <typename T>
 class resizable_stack : public stack<T> {
-  static const std::size_t DEFAULT_CAP = 10; 
+  static constexpr std::size_t DEFAULT_CAP = 10; 
 public:
   resizable_stack() : elems(new T[DEFAULT_CAP]),
     n(0),
     max_cap(DEFAULT_CAP) {}
   virtual ~resizable_stack();
-  virtual void push(const T elem) override;
-  virtual void push(const T elems[], int len) override;
+  virtual void push(T elem) override;
+  virtual void push(const T elems[], std::size_t len) override;
   virtual T pop() override;
 private:
-  void resize(const std::size_t cap);
+  void resize(std::size_t cap);
   T *elems;
   std::size_t n, max_cap;
 };
+
+}
+// END NAMESPACE intrnl_ds
 
 // TEMPLATE CLASS stack
 template <typename T>
 template <std::size_t SIZE>
 stack<T>* stack<T>::make_fixed() {
-  if (SIZE <= 0) throw std::invalid_argument("Size of queue must be > 0");
-  else return new fixed_stack<
+  if (SIZE <= 0) throw std::invalid_argument("Size of stack must be > 0");
+  else return new intrnl_ds::fixed_stack<
     T,
     SIZE>{}; 
 }
 
 template <typename T>
 stack<T>* stack<T>::make_resizable() {
-  return new resizable_stack<T>();
+  return new intrnl_ds::resizable_stack<T>{};
+}
+
+namespace intrnl_ds {
+
+// TEMPLATE CLASS fixed_stack
+template <typename T, std::size_t SIZE>
+intrnl_ds::fixed_stack<T,SIZE>::~fixed_stack() {}
+
+template <typename T, std::size_t SIZE>
+inline void intrnl_ds::fixed_stack<T,SIZE>::push(T elem) {
+  check_bounds(n);
+  elems[n++] = elem;
+}
+
+template <typename T, std::size_t SIZE>
+inline void intrnl_ds::fixed_stack<T,SIZE>::push(
+  const T elems[],
+  std::size_t len)
+{
+  for (std::size_t i = 0; i < len; i++) push(elems[i]);  
+}
+
+template <typename T, std::size_t SIZE>
+inline T intrnl_ds::fixed_stack<T,SIZE>::pop() {
+  return elems[--n];
+}
+
+template <typename T, std::size_t SIZE>
+inline void fixed_stack<T,SIZE>::check_bounds(std::size_t i) const {
+  if (i >= n) throw std::out_of_range("Illegal index " + i);
 }
 
 // TEMPLATE CLASS resizable_stack
 template <typename T>
-resizable_stack<T>::~resizable_stack() {
+intrnl_ds::resizable_stack<T>::~resizable_stack() {
   delete[] elems;
 }
 
 template <typename T>
-void resizable_stack<T>::push(const T elem) {
+void intrnl_ds::resizable_stack<T>::push(T elem) {
   if (n == max_cap) resize(max_cap*2); 
   elems[n++] = elem;
 }
 
 template <typename T>
-void resizable_stack<T>::push(
+void intrnl_ds::resizable_stack<T>::push(
   const T elems[],
-  int len) 
+  std::size_t len) 
 {
   for (std::size_t i = 0; i < len; i++) push(elems[i]); 
 }
   
 template <typename T>
-T resizable_stack<T>::pop() {
+T intrnl_ds::resizable_stack<T>::pop() {
   T elem = elems[--n];
   if (n > 0 && n <= max_cap/4) resize(max_cap/2);
   return elem;
 } 
 
 template <typename T>
-void resizable_stack<T>::resize(const std::size_t cap) {
+void intrnl_ds::resizable_stack<T>::resize(std::size_t cap) {
   T *a = new T[cap];
   for (auto i = 0; i < n; i++) {
     a[i] = elems[i];
   }
   delete[] elems;
   elems = a;
+  max_cap = cap;
 }
 
-#endif // __QUEUE_H_INCLUDED__
+}
+// END NAMESPACE intrnl_ds
+
+#endif // __STACK_H_INCLUDED__
---
+++ test_fixed_stack.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include "stack.h"
+
+int main() {
+  constexpr int STACK_SZ = 5;
+  auto stk = stack<int>::make_fixed<STACK_SZ>();
+  int elems[STACK_SZ]{
+    1,
+    2,
+    3,
+    4,
+    5};
+  stk->push(elems, STACK_SZ);
+  std::cout << stk->pop() << ',' << stk->pop() << '\n';
+  return 0;
+}
